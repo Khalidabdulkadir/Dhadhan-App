36b5835d1f6c9b5570841ad4271e007d00bbcfc3
khalid abdulkadir <hanyak65@gmail.com>
2025-12-13 01:39:58 +0300

    Add STATICFILES_DIRS to serve frontend assets

--- backend/matrix_backend/settings.py
+++ backend/matrix_backend/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'  # content: ensure leading slash
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
