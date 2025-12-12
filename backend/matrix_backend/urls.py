538d5a35e0ad5bb8ce6fa9859f7d75402aa85530
khalid abdulkadir <hanyak65@gmail.com>
2025-12-13 01:46:59 +0300

    Add redirect for /admin to /admin/

--- backend/matrix_backend/urls.py
+++ backend/matrix_backend/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Redirect /admin to /admin/ to prevent React from capturing it
+    path('admin', RedirectView.as_view(url='/admin/', permanent=True)),
     path('api/', include('api.urls')),
     # Match all other routes to index.html for SPA
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
